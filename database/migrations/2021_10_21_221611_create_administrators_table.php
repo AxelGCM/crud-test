5da74ecb3edb410a137599025c19c75775041274
axelc <tailor.1999agcm@gmail.com>
2021-10-21 17:46:38 -0500

    Inicialización del proyecto en Laravel

---
+++ database/migrations/2021_10_21_221611_create_administrators_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdministratorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('administrators', function (Blueprint $table) {
+            $table->id();
+            $table->string('icon');
+            $table->string('names');
+            $table->string('last_name');
+            $table->string('email');
+            $table->string('area');
+            $table->boolean('status');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('administrators');
+    }
+}
